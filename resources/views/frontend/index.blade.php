3d84e246128ee7f23a07186c11886b2947bf650e
Shakil Hossen <shakils923@gmail.com>
2023-05-18 13:40:34 +0600

    some design change

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -6,7 +6,8 @@
 @section('content')
     {{-- @include('layouts.frontend.slider') --}}
     <div class="home-main">
-<a href="{{ URL::to('/admin') }}" class="admin-btn">@lang('english.GO_TO_ADMIN_PANEL') <i class="fa fa-play admin-icon"></i></a>
+        <a href="{{ URL::to('/admin') }}" class="admin-btn">@lang('english.GO_TO_ADMIN_PANEL')
+            <i class="fa fa-arrow-right admin-icon"></i></a>
 
         {{-- @include('layouts.frontend.home_content') --}}
     </div>
